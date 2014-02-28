16437c37091386c8818ea9c0153018914a1d9ca9
ChengTszTung <yiptsztung@gmail.com>
2014-03-01 00:51:25 +0800

    Initial Commit

---
+++ Smart Transaction/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Smart Transaction
+//
+//  Created by TszTung Cheng on 1/3/14.
+//  Copyright (c) 2014 1314-114102-02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Smart Transaction/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Smart Transaction
+//
+//  Created by TszTung Cheng on 1/3/14.
+//  Copyright (c) 2014 1314-114102-02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
